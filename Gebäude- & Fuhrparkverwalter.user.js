41f8bd05b8b8213be6a7f864a26ef5166fa2f472
Sobol0202 <67016865+Sobol0202@users.noreply.github.com>
2025-10-29 18:24:36 +0100

    Update Gebäude- & Fuhrparkverwalter.user.js

--- Gebäude- & Fuhrparkverwalter.user.js
+++ Gebäude- & Fuhrparkverwalter.user.js
@@ -13,7 +13,7 @@
     // @run-at      document-idle
     // @grant       none
     // @resource    https://forum.leitstellenspiel.de/index.php?thread/23925-script-geb%C3%A4ude-fuhrparkverwalter-by-bos-ernie/
-    // @require     https://update.greasyfork.org/scripts/491642/1354160/%2A%20Common%3A%20IndexedDB.js
+    // @require     https://github.com/Sobol0202/BOS-Ernies-LSS-Scripte/raw/refs/heads/main/Commons/Common:%20IndexedDB.js
     // ==/UserScript==
      
     /* global $, user_premium */
